f31473c42f18b448e4f9be66164c777c07085de9
adeliadanjou <princessadelia@gmail.com>
2019-10-14 13:27:31 +0200

    construyendo primera app de springboot

---
+++ src/test/java/com/spring/primerspringboot/PrimerSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.primerspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrimerSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
